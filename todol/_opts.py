4394e01de266b8d7eec7236c42350c5b85a3c614
Bryan Hu <bryan.hu.2020@gmail.com>
2020-12-27 17:47:44 -0800

    :recycle: Refactor due date options into _opts.py

--- todol/_opts.py
+++ todol/_opts.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# type: ignore
 """
 Initial author: Bryan Hu.
 
@@ -12,8 +11,11 @@ A file of parsers for argparse
 
 """
 import argparse
+from typing import Any, Callable, Optional, Sequence, Tuple
 
-__all__ = ["color_options"]
+from . import _utils
+
+__all__ = ["color_options", "due_date_options"]
 color_options: argparse.ArgumentParser = argparse.ArgumentParser(add_help=False)
 
 
@@ -23,15 +25,15 @@ class BooleanOptionalAction(argparse.Action):
     # pylint: disable=C,R,W
     def __init__(
         self,
-        option_strings,
-        dest,
-        default=None,
-        type=None,
-        choices=None,
-        required=False,
-        help=None,
-        metavar=None,
-    ):
+        option_strings: Tuple[str, ...],
+        dest: str,
+        default: Optional[Any] = None,
+        type: Optional[Callable[[str], Any]] = None,
+        choices: Optional[Sequence[str]] = None,
+        required: bool = False,
+        help: Optional[str] = None,
+        metavar: Optional[str] = None,
+    ) -> None:
 
         _option_strings = []
         for option_string in option_strings:
@@ -48,19 +50,20 @@ class BooleanOptionalAction(argparse.Action):
             option_strings=_option_strings,
             dest=dest,
             nargs=0,
-            default=default,
-            type=type,
+            default=default,  # type: ignore
+            type=type,  # type: ignore
             choices=choices,
             required=required,
             help=help,
             metavar=metavar,
         )
 
-    def __call__(self, parser, namespace, values, option_string=None):
+    def __call__(self, parser, namespace: argparse.Namespace, values, option_string: Optional[str] = None) -> None:  # type: ignore
         if option_string in self.option_strings:
+            assert isinstance(option_string, str)
             setattr(namespace, self.dest, not option_string.startswith("--no-"))
 
-    def format_usage(self):
+    def format_usage(self) -> str:
         return " | ".join(self.option_strings)
 
 
@@ -74,4 +77,23 @@ opts.add_argument(
     action="store_true",
     help="Force color output even if not printing to a terminal",
     dest="force_color",
+)
+due_date_options: argparse.ArgumentParser = argparse.ArgumentParser(add_help=False)
+due_dates = due_date_options.add_mutually_exclusive_group()
+due_dates.add_argument(  # TODO: Add time capability
+    "--due",
+    "--due-date",
+    "-d",
+    default=str(_utils.tomorrow),
+    type=str,
+    help="The due date in ISO 8601 format, YYYY-MM-DD (padded with zeros, if required)",
+    dest="due_date",
+)
+due_dates.add_argument(
+    "--long-term",
+    "--long",
+    "-l",
+    action="store_true",
+    help="Make the todo a long term goal",
+    dest="long_term",
 )
