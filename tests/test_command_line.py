34bb85f3652c77e9990a86d3a29dd477e391651b
Bryan Hu <bryan.hu.2020@gmail.com>
2020-12-28 17:34:00 -0800

    :white_check_mark: :see_no_evil: Made tests better

--- tests/test_command_line.py
+++ tests/test_command_line.py
@@ -1,17 +1,40 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # pylint: disable=C,R0201,R0903
+import os
+import shutil
 import subprocess
 import sys
+from copy import deepcopy
 from pathlib import Path
 
-main_path = Path(__file__).parent.parent.joinpath("todol").joinpath("__main__.py")
-run_todol = [sys.executable, "-m", "todol"]
+project_dir = Path(__file__).parent.parent
+CUR_PY = str(sys.executable)
+
+todol_test_dir = project_dir.joinpath("todol_test_dir")
+shutil.rmtree(str(todol_test_dir), ignore_errors=True)
+
+todol_fake_rc_file = project_dir.joinpath("todol_test_rc_file")
+if todol_fake_rc_file.exists():
+    os.remove(todol_fake_rc_file)
+
+MODIFIED_ENV = deepcopy(os.environ).update(
+    {
+        "TODOL_CONFIG_DIR": str(todol_test_dir),
+        "RC_FILE": str(todol_fake_rc_file),
+    }
+)
+
+main_path = project_dir.joinpath("todol").joinpath("__main__.py")
 
 
 def _run_cmd(cmd, input_=None):
     return subprocess.run(
-        cmd, cwd=str(main_path.parent.parent), check=True, input=input_
+        cmd,
+        cwd=str(main_path.parent.parent),
+        check=True,
+        input=input_,
+        env=MODIFIED_ENV,
     )
 
 
@@ -20,40 +43,24 @@ def test_meta():
 
 
 def test_version():
-    assert _run_cmd([sys.executable, "-m", "todol", "--version"]).returncode == 0
+    assert _run_cmd([CUR_PY, "-m", "todol", "--version"]).returncode == 0
 
 
-class TestHelp:
-    def test_main(self):
-        assert _run_cmd([sys.executable, "-m", "todol", "--help"]).returncode == 0
-
-    def test_list(self):
-        assert (
-            _run_cmd([sys.executable, "-m", "todol", "list", "--help"]).returncode == 0
-        )
-
-    def test_add(self):
-        assert (
-            _run_cmd([sys.executable, "-m", "todol", "add", "--help"]).returncode == 0
-        )
-
-    def test_remove(self):
-        assert (
-            _run_cmd([sys.executable, "-m", "todol", "remove", "--help"]).returncode
-            == 0
-        )
-
-    def test_init(self):
-        assert (
-            _run_cmd([sys.executable, "-m", "todol", "init", "--help"]).returncode == 0
-        )
-
-    def test_finish(self):
-        assert (
-            _run_cmd([sys.executable, "-m", "todol", "finish", "--help"]).returncode
-            == 0
-        )
+def test_help():
+    for command in [None, "list", "init", "add", "remove", "finish", "complete"]:
+        for color_option in [None, "--force-color", "--no-color"]:  # Test color options
+            args = [CUR_PY, "-m", "todol"]
+            if command:
+                args.append(command)
+            args.append("--help")
+            if color_option:
+                args.append(color_option)
+            assert _run_cmd(args).returncode == 0
 
 
-# class TestInit:  # TODO: Write tests for subcommands
-#     ...
+class TestInit:  # TODO: Write tests for subcommands
+    def test_no_shell(self):
+        assert _run_cmd([CUR_PY, "-m", "todol", "init", "--no-shell"]).returncode == 0
+
+    def test_add_shell(self):  # TODO(ThatXliner): Add an --only-shell option
+        assert _run_cmd([CUR_PY, "-m", "todol", "init"], input_=b"y\n").returncode == 0
